36fe6887451d33e210dac6862f2480e5bccf71b3
jumpe.katayama <junktym123@gmail.com>
2021-03-11 00:15:48 +0900

    Set up project

---
+++ CleanseSample/CleanseSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanseSample
+//
+//  Created by Jumpei Katayama on 2021/03/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
